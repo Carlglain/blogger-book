7501eac1dac919f6a7852a0ca11301f584be0744
Carlstorm <carlglainnyuysemo@gmail.com>
2025-11-28 06:25:52 +0100

    feat:added the get post by id endpoint

--- controllers/Posts.js
+++ controllers/Posts.js
@@ -3,10 +3,15 @@ const allPosts = async (req, res) => {
   const listOfPosts = await Posts.findAll();
   res.json(listOfPosts);
 };
+const specificPost = async (req, res) => {
+  const id = req.params.id;
+  const post = await Posts.findByPk(id);
+  res.status(200).send(post);
+};
 const createPost = async (req, res) => {
   const post = req.body;
   await Posts.create(post);
   res.json(post);
 };
 
-module.exports = { allPosts, createPost };
+module.exports = { allPosts, createPost, specificPost };
--- routes/Posts.js
+++ routes/Posts.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { allPosts, createPost } = require("../controllers/Posts.js");
+
+const {
+  allPosts,
+  createPost,
+  specificPost,
+} = require("../controllers/Posts.js");
 router.get("/", allPosts);
+router.get("/:id", specificPost);
 router.post("/create", createPost);
 module.exports = router;
